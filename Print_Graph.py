33ac24db4cba0c5ca9cc0020cf8b84fc503f5305
Michał Pawlik <pawlikm11@gmail.com>
2024-05-11 22:37:52 +0200

    Jakimś cudem działa(jeszcze)3 Dodana macierz i lista. działa też pomir czasu. Wszystko powinno działać po testach na małej ilości danych

--- Print_Graph.py
+++ Print_Graph.py
@@ -1,38 +1,35 @@
 import networkx as nx
 import matplotlib.pyplot as plt
 
-def create_and_draw_graph(edges, source):
+def draw_graph_with_paths(edges, source):
     G = nx.DiGraph()
-    for start, end, weight in edges:
-        G.add_edge(start, end, weight=weight)
 
-    # Wyznaczanie najkrótszych ścieżek z użyciem algorytmu Dijkstry
+    for u, v, weight in edges:
+        G.add_edge(u, v, weight=weight)
+
+    # Obliczanie najkrótszych ścieżek
     path_lengths = nx.single_source_dijkstra_path_length(G, source)
     paths = nx.single_source_dijkstra_path(G, source)
 
-    # Ustawienia wizualne dla grafu
-    pos = nx.spring_layout(G)
+    pos = nx.spring_layout(G)  # Pozycje dla wizualizacji
+    nx.draw_networkx_nodes(G, pos, node_color='lightblue', node_size=500)
+    nx.draw_networkx_labels(G, pos)
+    nx.draw_networkx_edges(G, pos, edge_color='gray')
     edge_labels = nx.get_edge_attributes(G, 'weight')
-
-    # Rysowanie grafu
-    nx.draw(G, pos, with_labels=True, node_color='skyblue', node_size=700, edge_color='gray')
     nx.draw_networkx_edge_labels(G, pos, edge_labels=edge_labels)
 
-    # Wyróżnianie najkrótszych ścieżek
+    # Rysowanie najkrótszych ścieżek czerwonym kolorem
     for target, path in paths.items():
-        if target != source:  # Pomijamy wierzchołek źródłowy
-            path_edges = list(zip(path[:-1], path[1:]))
-            nx.draw_networkx_edges(G, pos, edgelist=path_edges, edge_color='red', width=2)
-            nx.draw_networkx_nodes(G, pos, nodelist=path, node_color='red')
+        path_edges = list(zip(path[:-1], path[1:]))
+        nx.draw_networkx_edges(G, pos, edgelist=path_edges, edge_color='red', width=2)
 
-    plt.title('Najkrótsze ścieżki z wierzchołka {}'.format(source))
+    plt.title('Graf z najkrótszymi ścieżkami od wierzchołka ' + str(source))
     plt.show()
 
-# Krawędzie w formacie (start, koniec, waga)
 edges = [
     (0, 1, 10), (0, 3, 5), (1, 2, 1), (1, 3, 2),
     (2, 4, 4), (3, 1, 3), (3, 2, 9), (3, 4, 2),
     (4, 0, 7), (4, 2, 6)
 ]
 
-create_and_draw_graph(edges, 0)  # 0 jest wierzchołkiem źródłowym
+draw_graph_with_paths(edges, 0)  # 0 to wierzchołek startowy dla algorytmu Dijkstry
--- main.cpp
+++ main.cpp
@@ -1,7 +1,7 @@
 //#include <iostream>
 //#include "AdjacencyList.h"
 //#include "Dijkstra.h"
-//
+////
 //int main() {
 //    int numVertices = 5;
 //    AdjacencyList graph(numVertices);
@@ -80,39 +80,118 @@
 //
 //    return 0;
 //}
+
+
+
+
+
+
+
+
+
+
+
+//#include <iostream>
+//#include <chrono>
+//#include "AdjacencyList.h"
+//#include "AdjacencyMatrix.h"
+//#include "Dijkstra.h"
+//#include "GraphGenerator.h"
+//
+//int main() {
+//    int numVertices = 100;  // Liczba wierzchołków w grafie  (np. 10, 50, 100, 500 i 1000)
+//    double density = 1;  // Gęstość grafu 25%, 50%, 75% oraz dla grafu pełnego
+//
+//    // Generowanie grafu jako lista sąsiedztwa
+//    Graph* listGraph = GraphGenerator::generateListGraph(numVertices, density);
+//    // Generowanie grafu jako macierz sąsiedztwa
+//    Graph* matrixGraph = GraphGenerator::generateMatrixGraph(numVertices, density);
+//
+//    // Mierzenie czasu wykonania algorytmu Dijkstry dla listy sąsiedztwa
+//    auto startList = std::chrono::high_resolution_clock::now();
+//    auto resultList = Dijkstra::computePaths(*listGraph, 0, numVertices);  // Użycie dereferencji
+//    auto endList = std::chrono::high_resolution_clock::now();
+//    std::chrono::duration<double, std::milli> elapsedList = endList - startList;
+//
+//    // Mierzenie czasu wykonania algorytmu Dijkstry dla macierzy sąsiedztwa
+//    auto startMatrix = std::chrono::high_resolution_clock::now();
+//    auto resultMatrix = Dijkstra::computePaths(*matrixGraph, 0, numVertices);  // Użycie dereferencji
+//    auto endMatrix = std::chrono::high_resolution_clock::now();
+//    std::chrono::duration<double, std::milli> elapsedMatrix = endMatrix - startMatrix;
+//
+//    std::cout << "Czas wykonania Dijkstry dla listy: " << elapsedList.count() << " ms" << std::endl;
+//    std::cout << "Czas wykonania Dijkstry dla macierzy: " << elapsedMatrix.count() << " ms" << std::endl;
+//
+//    delete listGraph;
+//    delete matrixGraph;
+//
+//    return 0;
+//}
+
+
+
+
+
+//TEST POPRAWNOSCI
 #include <iostream>
-#include <chrono>
 #include "AdjacencyList.h"
 #include "AdjacencyMatrix.h"
 #include "Dijkstra.h"
-#include "GraphGenerator.h"
 
 int main() {
-    int numVertices = 100;  // Liczba wierzchołków w grafie
-    double density = 1;  // Gęstość grafu
+    int numVertices = 5;
 
-    // Generowanie grafu jako lista sąsiedztwa
-    Graph* listGraph = GraphGenerator::generateListGraph(numVertices, density);
-    // Generowanie grafu jako macierz sąsiedztwa
-    Graph* matrixGraph = GraphGenerator::generateMatrixGraph(numVertices, density);
+    // Utworzenie i inicjalizacja grafu jako lista sąsiedztwa
+    AdjacencyList listGraph(numVertices);
+    listGraph.addEdge(0, 1, 10);
+    listGraph.addEdge(0, 3, 5);
+    listGraph.addEdge(1, 2, 1);
+    listGraph.addEdge(1, 3, 2);
+    listGraph.addEdge(2, 4, 4);
+    listGraph.addEdge(3, 1, 3);
+    listGraph.addEdge(3, 2, 9);
+    listGraph.addEdge(3, 4, 2);
+    listGraph.addEdge(4, 0, 7);
+    listGraph.addEdge(4, 2, 6);
 
-    // Mierzenie czasu wykonania algorytmu Dijkstry dla listy sąsiedztwa
-    auto startList = std::chrono::high_resolution_clock::now();
-    auto resultList = Dijkstra::computePaths(*listGraph, 0, numVertices);  // Użycie dereferencji
-    auto endList = std::chrono::high_resolution_clock::now();
-    std::chrono::duration<double, std::milli> elapsedList = endList - startList;
+    // Utworzenie i inicjalizacja grafu jako macierz sąsiedztwa
+    AdjacencyMatrix matrixGraph(numVertices);
+    matrixGraph.addEdge(0, 1, 10);
+    matrixGraph.addEdge(0, 3, 5);
+    matrixGraph.addEdge(1, 2, 1);
+    matrixGraph.addEdge(1, 3, 2);
+    matrixGraph.addEdge(2, 4, 4);
+    matrixGraph.addEdge(3, 1, 3);
+    matrixGraph.addEdge(3, 2, 9);
+    matrixGraph.addEdge(3, 4, 2);
+    matrixGraph.addEdge(4, 0, 7);
+    matrixGraph.addEdge(4, 2, 6);
 
-    // Mierzenie czasu wykonania algorytmu Dijkstry dla macierzy sąsiedztwa
-    auto startMatrix = std::chrono::high_resolution_clock::now();
-    auto resultMatrix = Dijkstra::computePaths(*matrixGraph, 0, numVertices);  // Użycie dereferencji
-    auto endMatrix = std::chrono::high_resolution_clock::now();
-    std::chrono::duration<double, std::milli> elapsedMatrix = endMatrix - startMatrix;
+    // Testowanie algorytmu Dijkstry dla listy sąsiedztwa
+    auto [distancesList, predecessorsList] = Dijkstra::computePaths(listGraph, 0, numVertices);
+    std::cout << "Lista sasiedztwa:" << std::endl;
+    for (int i = 0; i < numVertices; ++i) {
+        std::cout << "Odleglosc od 0 do " << i << ": " << distancesList[i] << ", Sciezka: ";
+        int step = i;
+        while (predecessorsList[step] != -1) {
+            step = predecessorsList[step];
+            std::cout << " <- " << step;
+        }
+        std::cout << std::endl;
+    }
 
-    std::cout << "Czas wykonania Dijkstry dla listy: " << elapsedList.count() << " ms" << std::endl;
-    std::cout << "Czas wykonania Dijkstry dla macierzy: " << elapsedMatrix.count() << " ms" << std::endl;
-
-    delete listGraph;
-    delete matrixGraph;
+    // Testowanie algorytmu Dijkstry dla macierzy sąsiedztwa
+    auto [distancesMatrix, predecessorsMatrix] = Dijkstra::computePaths(matrixGraph, 0, numVertices);
+    std::cout << "Macierz sasiedztwa:" << std::endl;
+    for (int i = 0; i < numVertices; ++i) {
+        std::cout << "Odleglosc od 0 do " << i << ": " << distancesMatrix[i] << ", Sciezka: ";
+        int step = i;
+        while (predecessorsMatrix[step] != -1) {
+            step = predecessorsMatrix[step];
+            std::cout << " <- " << step;
+        }
+        std::cout << std::endl;
+    }
 
     return 0;
 }
